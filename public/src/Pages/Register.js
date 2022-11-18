03ba09511d128f2f9a98782013d7573e6887816f
Lelabeshidze <94396022+Lelabeshidze@users.noreply.github.com>
2022-11-18 21:42:55 +0400

    added basic styles

--- public/src/Pages/Register.js
+++ public/src/Pages/Register.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 function Register() {
-  return (
-    <div>Register</div>
-  )
-}
+  const handleSubmit = (e) => {
+    exports.preventDefault();
+    alert("form");
+  };
+  const handleChange = (e ) => {
 
-export default Register
+  }
+  return (
+    <>
+      <FormContainer>
+        <form onSubmit={() => handleSubmit()}>
+          <div className="brand">
+            <img src="" alt="" />
+            <h1>snappy</h1>
+          </div>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="email"
+            placeholder="email"
+            name="email"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            name="password"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            onChange={(e) => handleChange(e)}
+          />
+          <button type="submit">Create User</button>
+          <span>
+            Already have an account ? <Link to="/login">Login</Link>{" "}
+          </span>
+        </form>
+      </FormContainer>
+    </>
+  );
+}
+const FormContainer = styled.div``;
+
+export default Register;
