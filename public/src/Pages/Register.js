bd6c2dc1511a9f1d7553364f9443dc6d2484a400
Lelabeshidze <94396022+Lelabeshidze@users.noreply.github.com>
2022-11-19 19:38:50 +0400

    register functionality

--- public/src/Pages/Register.js
+++ public/src/Pages/Register.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Logo from "../assets/logo.svg";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -13,6 +13,7 @@ function Register() {
     password: "",
     confirmPassword: "",
   });
+  const navigate = useNavigate();
 
   const toastOptions = {
     position: "bottom-right",
@@ -24,12 +25,19 @@ function Register() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (handleValidation()) {
-      const { username, email, password, confirmPassword } = values;
+      const { username, email, password } = values;
       const { data } = await axios.post(registerRoute, {
         username,
         email,
         password,
       });
+      if (data.status === false) {
+        toast.error(data.msg, toastOptions);
+      }
+      if (data.status === true) {
+        localStorage.setItem("chat-app-user", JSON.stringify(data.user));
+      }
+      navigate("/");
     }
   };
 
--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,3 +1,26 @@
-module.exports.register = (req, res, next) => {
-  console.log(req.body);
+const User = require("../model/userModel");
+const bcrypt = require("bcrypt");
+
+module.exports.register = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+    const usernameCheck = await User.findOne({ username });
+    if (usernameCheck)
+      return res.json({ msg: "Username is already used", status: false });
+
+    const emailCheck = await User.findOne({ email });
+    if (emailCheck)
+      return res.json({ msg: "Email  is already used", status: false });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      email,
+      username,
+      password: hashedPassword,
+    });
+    delete user.password;
+    return res.json({ status: true, user });
+  } catch (ex) {
+    next(ex);
+  }
 };
