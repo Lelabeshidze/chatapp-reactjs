5b712b826896e33c9f0c5f81b794177459dab9d3
Lelabeshidze <94396022+Lelabeshidze@users.noreply.github.com>
2022-11-20 22:08:03 +0400

    current user part

--- public/src/Components/Contacts.js
+++ public/src/Components/Contacts.js
@@ -42,7 +42,7 @@ function Contacts({ contacts, currentUser }) {
               );
             })}
           </div>
-          <div className="current-avatar">
+          <div className="current-user">
             <div className="avatar">
               <img
                 src={`data:image/svg+xml;base64,${currentUserImage}`}
@@ -50,9 +50,10 @@ function Contacts({ contacts, currentUser }) {
               />
             </div>
             <div className="username">
-              <h3>{currentUserName}</h3>
+              <h2>{currentUserName}</h2>
             </div>
           </div>
+          
         </Container>
       )}
     </>
@@ -60,52 +61,88 @@ function Contacts({ contacts, currentUser }) {
 }
 
 const Container = styled.div`
-display: grid;
-grid-template-rows: 10% 75% 15%;
-overflow: hidden;
-background-color: #080420;
-.brand {
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-  justify-content: center;
-  img {
-    height: 2rem;
-  }
-  h3 {
-    color: white;
-    text-transform: uppercase;
-  }
-}
-.contacts {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  overflow: auto;
-  gap: 0.8rem;
-  &::-webkit-scrollbar {
-    width: 0.2rem;
-    &-thumb {
-      background-color: #ffffff39;
-      width: 0.1rem;
-      border-radius: 1rem;
+  display: grid;
+  grid-template-rows: 10% 75% 15%;
+  overflow: hidden;
+  background-color: #080420;
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img {
+      height: 2rem;
+    }
+    h3 {
+      color: white;
+      text-transform: uppercase;
     }
   }
-  .contact {
-    background-color: #ffffff34;
-    min-height: 5rem;
-    cursor: pointer;
-    width: 90%;
-    border-radius: 0.2rem;
-    padding: 0.4rem;
+  .contacts {
     display: flex;
-    gap: 1rem;
+    flex-direction: column;
     align-items: center;
-    transition: 0.5s ease-in-out;
-    .avatar {
-      img {
-        height: 3rem;
+    overflow: auto;
+    gap: 0.8rem;
+    &::-webkit-scrollbar {
+      width: 0.2rem;
+      &-thumb {
+        background-color: #ffffff39;
+        width: 0.1rem;
+        border-radius: 1rem;
       }
     }
+    .contact {
+      background-color: #ffffff34;
+      min-height: 5rem;
+      cursor: pointer;
+      width: 90%;
+      border-radius: 0.2rem;
+      padding: 0.4rem;
+      display: flex;
+      gap: 1rem;
+      align-items: center;
+      transition: 0.5s ease-in-out;
+      .avatar {
+        img {
+          height: 3rem;
+        }
+      }
+      .username {
+        h3 {
+          color: white;
+        }
+      }
+    }
+    .selected {
+      background-color: #9a86f3;
+    }
+  }
+  .current-user {
+    background-color: #0d0d30;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+    .avatar {
+      img {
+        height: 4rem;
+        max-inline-size: 100%;
+      }
+    }
+    .username {
+      h2 {
+        color: white;
+      }
+    }
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      gap: 0.5rem;
+      .username {
+        h2 {
+          font-size: 1rem;
+        }
+      }
+    }
+  }
 `;
 export default Contacts;
